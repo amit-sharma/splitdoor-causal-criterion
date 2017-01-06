22ce2976f14f92bc92054fa2a6c780f45b37d7be
amit-sharma <amit_sharma@live.com>
2017-01-06 17:56:42 -0500

    added comments for examples

--- splitdoor/splitdoor_estimate_example.R
+++ splitdoor/splitdoor_estimate_example.R
@@ -1,36 +1,50 @@
 library(splitdoor)
 library(dplyr)
 
+# A data.frame in the following format.
 load("../../binary-iv/Rscripts/splitdoor/recommender.log")
+
+# Optional: Sampling for faster run.
 tseries_df = recommender.log  %>%
   slice(1:30000)
+
+# Optional: Splitting data based on a user-specified time-interval.
 tseries_df = assign_time_periods(tseries_df, time_period = 15)
 
+# 1a. Computing a naive estimate first using correlations.
 naive_estimate_df = correlational_estimate(tseries_df,
                                            test_time_period=15,
                                            fn_add_time_period_factor = assign_time_periods)
+# 1b. Aggregating to each unique treatment
 agg_naive_estimate_df = aggregate_by_treatment_id(naive_estimate_df)
 
-
+# 2a. Estimating causal effect using the split-door criterion.
 causal_estimate_df = splitdoor_causal_estimate(tseries_df,
                                              fn_add_time_period_factor=assign_time_periods,
                                              fn_independence_test=fisher_independence_test,
                                              test_time_period=15,
                                              num_discrete_levels=4,
                                              independence_threshold=0.05)
+# 2b. Aggregating to each unique treatment
 agg_causal_estimate_df = aggregate_by_treatment_id(causal_estimate_df)
 
-    plot_invalid_splitdoors(notvalid_tseries_tbl)
-    plot_valid_splitdoors(valid_tseries_tbl)
-
-    indep_x=plot_sample_xy_pairs(valid_tseries_tbl) # commenting for loop
-    dep_x=plot_sample_xy_pairs(filter(notvalid_tseries_tbl, is_constant_demand==FALSE)) # commenting for loop
-
-
+# 3. Analysis of the obtained estimates
+# 3a. Comparing correlational and split-door estimates
 compare_splitdoor_correlational_estimate(agg_causal_estimate_df, agg_naive_estimate_df,
                                          by_group=TRUE, estimate_colname="agg_causal_estimate")
-check_distribution_splitdoors_by_group(causal_estimate_df)
 
+# 3b. Visualizing valid split-door <treatment, outcome> pairs
 inspect_splitdoor_pairs(filter(causal_estimate_df,pass_splitdoor_criterion),
                         tseries_df,
                         n_treatment_outcome_pairs = 4)
+
+#3c. Checking robustness of the split-door estimate to
+#    change in threshold for independence test.
+check_robustness_independence_threshold(tseries_df,
+                                        independence_thresholds = c(0.05,0.01,0.001))
+
+
+# 3d. Checking whether distribution of sample treatments selected by the
+#     split-door criterion is similar to the overall distribution over treatment groups.
+check_distribution_splitdoors_by_group(causal_estimate_df)
+
--- splitdoor/splitdoor_recommender_system_example.R
+++ splitdoor/splitdoor_recommender_system_example.R
@@ -1,48 +1,64 @@
-library(splitdoor)
-#library(dplyr)
+# Reproducing results in the original paper
+# Split-door criterion for causal identification: Automatic search for
+# natural experiments. Amit Sharma, Jake Hofman, Duncan Watts.
+# https://arxiv.org/abs/1611.09414
 
+library(splitdoor)
+library(dplyr)
+
+## Loading data from a recommender system's logs
 load("../../binary-iv/Rscripts/splitdoor/recommender.log")
 tseries_df = recommender.log
 
+## Computing estimates
+
+# 1a. Computing a naive estimate first using correlations.
 naive_estimate_df = correlational_estimate(tseries_df,
                                            test_time_period=15,
                                            fn_add_time_period_factor = assign_time_periods)
+# 1b. Aggregating to each focal product.
 agg_naive_estimate_df = aggregate_by_treatment_id(naive_estimate_df)
 
-
+# 2a. Estimating causal effect using the split-door criterion.
 causal_estimate_df = splitdoor_causal_estimate(tseries_df,
                                              fn_add_time_period_factor=assign_time_periods,
                                              fn_independence_test=fisher_independence_test,
                                              test_time_period=15,
                                              num_discrete_levels=4,
                                              independence_threshold=0.05)
+# 2b. Aggregating to each focal product.
 agg_causal_estimate_df = aggregate_by_treatment_id(causal_estimate_df)
 
-compare_splitdoor_correlational_estimate(agg_causal_estimate_df, agg_naive_estimate_df,
-                                         by_group=TRUE, estimate_colname="agg_causal_estimate")
+## Now generating figures from the paper.
 
-check_robustness_independence_threshold(tseries_df)
-
-ordered_tgroups_vec = c("Book","eBooks","Toy", "Home","Sports","Apparel",
-                       "Wireless","DVD", "Health and Beauty","Shoes")
-check_distribution_splitdoors_by_group(causal_estimate_df,
-                                       ordered_treatment_groups_vec = ordered_tgroups_vec)
-
+# Figure 6
+# a) Left panel
 valid_examples = data.frame(
   date_factor = c("2013-10.1","2013-11.1"),
   treatment_tseries_id = c(10798,42836),#c("B00E9N67MO","B0090SI56Y"),
   outcome_tseries_id = c(20407, 772)#c("B00DZZ2KYQ", "B0034G4P80")
 )
-
 inspect_splitdoor_pairs(inner_join(causal_estimate_df,valid_examples,
                                    by=c("date_factor","treatment_tseries_id","outcome_tseries_id")),
                         tseries_df)
-
+# b) Right panel
 invalid_examples = data.frame(
     date_factor = c("2013-12.1","2013-09.1"),
     treatment_tseries_id = c(44085,42728),
     outcome_tseries_id = c(39019, 42879)
   )
-
 inspect_splitdoor_pairs(inner_join(causal_estimate_df,invalid_examples, by=c("date_factor","treatment_tseries_id","outcome_tseries_id")),
                         tseries_df)
+
+# Figure 7
+check_robustness_independence_threshold(tseries_df)
+
+# Figure 8
+compare_splitdoor_correlational_estimate(agg_causal_estimate_df, agg_naive_estimate_df,
+                                         by_group=TRUE, estimate_colname="agg_causal_estimate")
+
+# Figure 9
+ordered_tgroups_vec = c("Book","eBooks","Toy", "Home","Sports","Apparel",
+                        "Wireless","DVD", "Health and Beauty","Shoes")
+check_distribution_splitdoors_by_group(causal_estimate_df,
+                                       ordered_treatment_groups_vec = ordered_tgroups_vec)
